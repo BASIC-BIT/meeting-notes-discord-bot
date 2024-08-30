85be9d75270bb4209b6b2701f0df32ce8216268c
BASIC <BASIC.BIT.1001@gmail.com>
2024-08-30 00:09:55 -0400

    Add dummy express endpoint for health checks

--- src/index.ts
+++ src/index.ts
@@ -559,3 +559,20 @@ function combineAudioWithFFmpeg(userBuffers: string[], audioFilePath: string): P
 
 
 client.login(TOKEN);
+
+
+
+// The following is a web server that literally only exists to make AppRunner's health check happy
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Health check endpoint
+// @ts-ignore
+app.get('/', (req, res) => {
+    res.status(200).send('OK');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running and listening on port ${PORT}`);
+});
